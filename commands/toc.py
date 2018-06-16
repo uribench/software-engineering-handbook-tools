5243f710fa83aa1bc415f66277a26c93aa8125b5
uribench <uribench@gmail.com>
2018-06-16 19:58:13 +0000

    Add initial support for toc command

---
+++ commands/toc.py
@@ -0,0 +1,36 @@
+from lib.commandBase import CommandBase
+from lib.scanConfigNavigationTree import ScanConfigNavigationTree
+
+__version__ = '0.9.0'
+
+class Toc(CommandBase):
+    """
+    Compose a TOC of the Handbook navigation tree from configuration.
+
+    Usage:
+        toc [options]
+
+    Options:
+        -h --help       show this help message and exit
+        -v, --version   show the version and exit
+    """
+
+    def __init__(self, command_args={}, global_args={}):
+        """"""
+        super().__init__(command_args, global_args, version=__version__)
+
+    def execute(self):
+        """Entry point for the execution of this sub-command."""
+        self.processArgs()
+        self.scanTree = ScanConfigNavigationTree(self.siteRoot, self.verbose)
+        self.scanTree.scan(self.nodePerformer)
+
+    def processArgs(self):
+        """Process global_args and command_args."""
+
+        self.siteRoot = self.global_args['--root']
+        self.verbose = self.global_args['--verbose']
+
+    def nodePerformer(self, rootPath, rootName, rootOptions, rootChildrenNodes):
+        """"""
+        pass
